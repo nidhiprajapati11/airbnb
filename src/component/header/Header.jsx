d35bda21d97a82a042a8dd10eedb4c2709c9eef0
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2024-11-17 11:59:46 +0530

    first project

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Header.css'
+// import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+
+function Header() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <>
+            <div className='header'>
+                <Link to='/'>
+                    <img
+                        className="header__icon"
+                        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                        alt=""
+                    />
+                </Link>
+
+                <div className='header__center'>
+                    <input type="text" />
+                    <SearchIcon />
+                </div>
+
+                <div className='header__right'>
+                    <p>Become a host</p>
+                    <LanguageIcon />
+                    <Button
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        <ExpandMoreIcon />
+                          <Avatar />
+                    </Button>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu>                 
+                </div>
+            </div>
+           
+        </>
+    )
+}
+
+export default Header
