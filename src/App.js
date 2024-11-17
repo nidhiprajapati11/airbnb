c39ee5f8deaa340194da523b58810aef30a78c52
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2024-11-17 12:25:58 +0530

    intial changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/airbnb' element={<Home />} />
         <Route path='/Search' element={<Search />} />
       </Routes>
       <Footer />
